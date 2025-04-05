d10ed75315818aa8c78363ca83bea496cf2c5251
Marc Bourgoin <themard85@gmail.com>
2025-04-05 08:56:08 -0600

    gpt-utils: Add OTA support for Asus xrom partition
    * Observed on Z01R.
    
    Change-Id: Ica4fcb1fb3e22a73e1eacad074bc1ababa1c5e2c

--- gpt-utils/gpt-utils.h
+++ gpt-utils/gpt-utils.h
@@ -150,7 +150,8 @@ extern "C" {
 	"aes", \
 	"laf", \
 	"splash", \
-	"bootlocker"
+	"bootlocker", \
+	"xrom"
 
 #define AB_PTN_LIST PTN_SWAP_LIST, "boot", "system", "vendor", "odm", "modem", "bluetooth"
 #define BOOT_DEV_DIR    "/dev/block/bootdevice/by-name"
